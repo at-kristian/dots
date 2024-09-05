20599a261d7d6c1d9f5def57a78f5f3a1d54f7c3
at-kristian <zelman@tutanota.com>
2024-09-05 21:33:44 +0200

    Remove vim background

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,10 @@ vim.cmd([[colorscheme gruvbox]])
 vim.opt.clipboard = 'unnamedplus'
 
 vim.g.wiki_root = "~/Documents/Wiki"
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
